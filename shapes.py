a3213d19b14ba75eb799217b64e0bc5c13715aa5
RoundChess <108309882+RoundChess@users.noreply.github.com>
2024-02-21 18:33:23 -0800

    Update shapes.py

--- shapes.py
+++ shapes.py
@@ -1,8 +1,20 @@
+from tkinter import *
+
+tk = Tk()
+tk.title("Calculator")
+
+canvas = Canvas(window, 300, 400)
+c.pack()
+
 class Shapes:
-	def __init__(self, x, y, width, height):
-    self.x = x
-		self.y = y
-		self.width = width
-		self.height = height
+	def __init__(self, x, y, width, height, col):
+	    self.x = x
+	    self.y = y
+	    self.width = width
+	    self.height = height
+		  self.col = col
 		
-		
+	def drawCircle():
+		canvas.create_oval(self.x, self.y, self.width, self.height, self.col)
+s = Shape(200, 200, 25, 25, #fffff)
+s.drawCircle()
